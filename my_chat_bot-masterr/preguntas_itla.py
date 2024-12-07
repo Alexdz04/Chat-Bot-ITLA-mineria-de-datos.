658aec5eadfa1486a3d5be18b8f167beed3525e7
Alejandro Diaz Peralta <alejandrodiazdp1@gmail.com>
2024-12-07 18:20:57 -0400

    Add project files.

---
+++ my_chat_bot-masterr/preguntas_itla.py
@@ -0,0 +1,16 @@
+def get_responses():
+    return [
+        ('Hola!!!!!! soy un chat box de preguntas del ITLA', ['hola', 'klk', 'saludos', 'buenas', 'eo'], True, []),
+        ('Estoy bien y tu?', ['estas', 'vas', 'vas'], False, ['como']),
+        ('Estos son los numeros de telefono del ITLA: 809-738-4852 / 809-793-2557', ['contacto', 'celular', 'telefono'], True, []),
+        ('El ITLA fue inagurado el 13 de agosto del año 2000 en el gobierno de Leonel Fernandez', ['fundacion', 'inaugurado', 'creacion'], True, []),
+        ('Este es el horario del ITLA: Lunes - viernes de 8:00 a. m. a 10:00 p. m. Sabados de 8:00 a.m a 4:00 p.m', ['horario', 'labora', 'abre', 'horario del itla'], False, []),
+        ('Las siglas ITLA son: Instituto Tecnologico de las Americas', ['siglas', 'letras', 'palabras'], True, []),
+        ('El rector actual del ITLA se llama Rafael Feliz Garcia', ['nombre', 'llama', 'rector'], True, []),
+        ('Para acceder al ITLA mayormente debes tener la mayoria de edad', ['quienes', 'personas', 'quien'], True, []),
+        ('Por cualquier cosa o cualquier inquetud, se puede llamar a servicio al cliente al 809-738-4852', ['orientar', 'dirijo', 'guiar'], True, []),
+        ('Si quieres pasar a pagar algo, como alguna materia, inscripcion, etc... puedes pasar por caja. Esta en el Edificio 3. El de la biblioteca', ['pagar', 'paga', 'caja', 'inscripcion', 'materia'], False, []),
+        ('Segun lo que dice el articulo 49 ningun estudiante puede retirar una asignatura mas de dos veces', ['retirar', 'ocasiones', 'ocasionalmente'], True, []),
+        ('En el ITLA se imparten 15 carreras tecnologicas', ['carreras', 'tiene', 'posee', 'presenta', 'imparten'], False, []),
+        ('Siempre a la orden', ['gracias', 'te lo agradezco', 'gracias bro', 'thanks'], True, [])
+    ]
